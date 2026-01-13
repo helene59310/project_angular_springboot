c517dc78eaf0653a064a720d12a7c3d6d47da8fc
LnVsr59 <helene.vasseur1@viacesi.fr>
2026-01-13 12:29:25 +0100

    écriture d'une requête @Query en utilisant JPQL et test de cette requête

--- src/main/java/com/example/demo/repositories/ProduitRepository.java
+++ src/main/java/com/example/demo/repositories/ProduitRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entities.ProduitEntity;
 
@@ -11,5 +13,8 @@ public interface ProduitRepository extends JpaRepository<ProduitEntity, Long> {
     List<ProduitEntity> findByNomProduit(String nom);
     List<ProduitEntity> findByNomProduitContains(String nom);
     List<ProduitEntity> findByPrixProduit(Double prix);
+
+    @Query("select p from ProduitEntity p where p.nomProduit like %:nomProduit and p.prixProduit > :prixProduit")
+    List<ProduitEntity> findByNomPrix (@Param("nomProduit") String nomProduit,@Param("prixProduit") Double prixProduit);
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -28,7 +28,7 @@ class DemoApplicationTests {
 		System.out.println(produit);
 	}
 
-		@Test
+	@Test
 	public void testUpdateProduit() {
 		ProduitEntity produit = produitRepository.findById(5L).get();
 		produit.setNomProduit("PC Asus");
@@ -44,28 +44,37 @@ class DemoApplicationTests {
 	@Test
 	public void testFindAllProduits() {
 		List<ProduitEntity> produits = produitRepository.findAll();
-		for (ProduitEntity produit:produits)
-		System.out.println(produit);
+		for (ProduitEntity produit : produits)
+			System.out.println(produit);
 	}
 
 	@Test
 	public void testFindByNomProduit() {
 		List<ProduitEntity> produits = produitRepository.findByNomProduit("PC Hp");
-		for (ProduitEntity produit:produits)
-		System.out.println(produit);
+		for (ProduitEntity produit : produits)
+			System.out.println(produit);
 	}
 
 	@Test
 	public void testFindByNomProduitContains() {
 		List<ProduitEntity> produits = produitRepository.findByNomProduitContains("e");
-		for (ProduitEntity produit:produits)
-		System.out.println(produit);
+		for (ProduitEntity produit : produits)
+			System.out.println(produit);
 	}
 
 	@Test
 	public void testFindByPrixProduit() {
 		List<ProduitEntity> produits = produitRepository.findByPrixProduit(500.5);
-		for (ProduitEntity produit:produits)
-		System.out.println(produit);
+		for (ProduitEntity produit : produits)
+			System.out.println(produit);
 	}
+
+	@Test
+	public void testfindByNomPrix() {
+		List<ProduitEntity> prods = produitRepository.findByNomPrix("PC Asus", 900.5);
+		for (ProduitEntity p : prods) {
+			System.out.println(p);
+		}
+	}
+
 }
